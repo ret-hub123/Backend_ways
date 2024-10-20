6565b34abdca8b64c406eb1033a9e1135b025538
Roma Belyakov <tedcruz12345q@gmail.com>
2024-10-20 21:54:18 +0300

    Create templates for notion_cat, choice notions for category, settings url links

--- django_project/site_for_notes/notion/url_notion.py
+++ django_project/site_for_notes/notion/url_notion.py
@@ -4,6 +4,7 @@ from notion import views
 from site_for_notes_apllication import urls_app
 
 urlpatterns = [
-    re_path('notion/', views.notion_page, name = 'notion'),
+    path('index_notion/', views.index_notion, name = 'index_notion'),
+    path('notion_cat/<slug:cat_slug>/', views.notions_page, name = 'notion_cat'),
 
 ]
